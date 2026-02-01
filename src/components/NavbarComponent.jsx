ed90013eb00dc73b1087f916b43c9d6caa346801
SouvikRay-dev <Souvik.Ray@cognizant.com>
2026-02-01 20:05:44 +0530

    Updated the Search Course Feature such that When the course is searched and then clicked, it will open the CourseDetails page only when the user is logged in.

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,10 +1,4 @@
-import {
-    useCallback,
-    useEffect,
-    useMemo,
-    useRef,
-    useState,
-} from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import edLogo from "../assets/edLogo.png";
 import userLogo from "../assets/userLogo.png";
 import {
